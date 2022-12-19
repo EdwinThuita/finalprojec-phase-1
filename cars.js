fa275753cbd4a4f0ec1263678c2afb383a5cb380
git_EngMaina <thuitamaina90@gmail.com>
2022-12-19 22:15:55 +0300

    html edits

---
+++ cars.js
@@ -0,0 +1,21 @@
+const
+let cars=[]
+window.addEventListener("load",()=>{
+    fetch("", {
+        method: "GET",
+    })
+    .then((response) => response.json())
+    .then((data)=>{
+        console.log(data);
+    })
+    .catch((error)=>{
+        console.log(error):
+    });
+    
+});
+function populateMenu(cars){
+    for(let =0; 1 <cars.lent)
+    const car=cars[1]
+    listi
+
+}
